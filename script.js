bf852cddb42097650f757c1f859217c7ef6da33a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-04-22 22:23:55 -0300

    feat: mostrar tarjeta con ubicacion

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const URL_ENDPOINT = 'https://rickandmortyapi.com/api/';
 const URL_UBICACIONES = URL_ENDPOINT + 'location';
 
+const mostrarTarjeta = (ubicacion) => {
+    console.log(ubicacion);
+    const contenedor = document.getElementById("contenedor")
+    const li = document.createElement('li');
+
+    const titulo = document.createElement('h3');
+    titulo.innerText = ubicacion.name;
+    li.appendChild(titulo);
+
+    const descripcion = document.createElement('p');
+    descripcion.innerText = ubicacion.dimension;
+    li.appendChild(descripcion);
+
+    contenedor.appendChild(li);
+}
+
 fetch(URL_UBICACIONES)
 .then(data => data.json())
 .then(result => {
     const results = result.results;
-    console.log("results", results)
+    const primerResultado = results[0];
+    mostrarTarjeta(primerResultado);
 })
